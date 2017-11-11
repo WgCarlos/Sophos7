b2320a9073bcb0daf038c74e6d9ef8b48a543ad6
Carlos <carlos.silva9796@outlook.com>
2017-11-11 11:36:04 -0300

    Modal Login
    Criado o botão de login

--- index.php
+++ index.php
@@ -38,8 +38,38 @@
     					<a class="nav-link" href="#">Contato</a>
   					</li>
   					<li class="nav-item">
-    					<a class="nav-link disabled" href="#">Login</a>
-  					</li>
+    					<!-- Button trigger modal -->
+						<button type="button" class="btn btn-outline-secondary" data-toggle="modal" data-target="#exampleModal">
+ 						 Login
+						</button>
+						<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+						<!-- Modal -->
+						  <div class="modal-dialog" role="document">
+						    <div class="modal-content">
+						      <div class="modal-header">
+						        <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+						        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						          <span aria-hidden="true">&times;</span>
+						        </button>
+						      </div>
+						      <div class="modal-body">
+						        <form>
+						          <div class="form-group">
+    									<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
+						          </div>
+						          <div class="form-group">
+    								<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+						          </div>
+						        </form>
+						      </div>
+						      <div class="modal-footer">
+						        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+						        <button type="submit" class="btn btn-primary">Entrar</button>
+						      </div>
+						    </div>
+						  </div>
+						</div>
+					</li>
 				</ul>
  			</div>
 		</nav>
